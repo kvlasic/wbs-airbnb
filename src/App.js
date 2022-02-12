e180e0eec01b764d8f155cf2a5a85228ae8febff
kvlasic <kristijan.vlasic1@gmail.com>
2022-02-12 15:31:35 +0100

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [lsdata, setLsdata] = useState([]);
+  useEffect(() => {
+    fetch("https://my-strapi-server-1.herokuapp.com/api/living-spaces")
+      .then((res) => res.json())
+      .then((res) => setLsdata(res.data));
+  }, []);
+  console.log(lsdata);
+  return <div className="App"></div>;
 }
-
 export default App;
