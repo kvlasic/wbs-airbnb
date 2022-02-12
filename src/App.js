62d984162faa698d138b52c6e14f9dfa4884b35a
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-12 16:40:31 +0100

    added list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import SpacesList from "./components/SpacesList";
 
-function App() {
-  const [lsdata, setLsdata] = useState([]);
-  useEffect(() => {
-    fetch("https://my-strapi-server-1.herokuapp.com/api/living-spaces")
-      .then((res) => res.json())
-      .then((res) => setLsdata(res.data));
-  }, []);
-  console.log(lsdata);
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <SpacesList />;
+};
+
 export default App;
