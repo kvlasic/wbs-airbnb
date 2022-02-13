3da058d034280b61b98db99a2cf118aee5c4646c
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-13 16:54:03 +0100

    space inner card style

---
+++ src/components/styles/innerCard.js
@@ -0,0 +1,13 @@
+import { common } from "@mui/material/colors";
+
+const innerCardStyle = {
+  maxWidth: 300,
+  maxHeight: 400,
+  m: 2,
+  border: 1,
+  borderRadius: 6,
+  p: 2,
+  borderColor: common["white"],
+};
+
+export default innerCardStyle;
