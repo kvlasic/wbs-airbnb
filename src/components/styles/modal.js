edcb7776fc2aabb6b3834b2fa66841916dd71140
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-13 16:54:17 +0100

    modal basic style

---
+++ src/components/styles/modal.js
@@ -0,0 +1,11 @@
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "75%",
+  height: "75%",
+  borderRadius: 10,
+};
+
+export default modalStyle;
