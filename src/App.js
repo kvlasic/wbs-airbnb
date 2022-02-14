3a9dfb295cd90ce3a038d64ef1891aae48844dc2
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-14 14:19:11 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import SpacesList from "./components/SpacesList";
+import { Container } from "@mui/material";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import SpacesList from "./pages/SpacesList";
+import WelcomePage from "./pages/WelcomePage";
 
 const App = () => {
-  return <SpacesList />;
+  return (
+    <Fragment>
+      <Router>
+        <NavBar />
+        <Container sx={{ mt: 6 }} maxWidth="xl">
+          <Routes>
+            <Route path="/places" element={<SpacesList />} />
+            <Route path="/" element={<WelcomePage />} />
+          </Routes>
+        </Container>
+      </Router>
+    </Fragment>
+  );
 };
 
 export default App;
