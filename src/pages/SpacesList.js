168d4591850694380e4484a73948152ac1d697d7
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-14 14:18:38 +0100

    refactor to react router pages

---
+++ src/pages/SpacesList.js
@@ -0,0 +1,36 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import SpaceCard from "../components/SpaceCard";
+import Spinner from "../components/Spinner";
+
+const SpacesList = () => {
+  const [spaces, setSpaces] = useState([]);
+
+  useEffect(() => {
+    const requestSpaces = async () => {
+      const res = await fetch(
+        "https://my-strapi-server-1.herokuapp.com/api/living-spaces"
+      );
+      const data = await res.json();
+      setSpaces(data.data);
+    };
+    requestSpaces();
+  }, []);
+
+  return (
+    <Box m={4}>
+      {spaces.length === 0 ? (
+        <Spinner />
+      ) : (
+        <Grid container spacing={6} mt={2}>
+          {spaces.map((space) => (
+            <SpaceCard key={space.id} space={space} />
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
+};
+
+export default SpacesList;
